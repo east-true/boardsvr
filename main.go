e43e342c4beea1220add509fa6c7f27242444aa3
east-true <leik24@naver.com>
2023-12-07 08:16:07 +0900

    merge helper file

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"boardsvr/handler"
-	"boardsvr/helper"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+var headers map[string]string = map[string]string{
+	"Content-Type": "application/json; charset=utf-8",
+}
+
 func main() {
 	engine := gin.Default()
 
 	engine.Use(gin.Logger())
-	engine.Use(helper.Header)
+	engine.Use(func(ctx *gin.Context) {
+		for key, val := range headers {
+			reqVal := ctx.Request.Header.Get(key)
+			if reqVal != val {
+				ctx.Status(http.StatusBadRequest)
+				return
+			}
+		}
+	})
+
 	engine.GET("", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"main": "hello"})
 	})
