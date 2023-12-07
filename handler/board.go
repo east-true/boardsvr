300603005f343e5b73833916667eade5c8fb0fb2
east-true <leik24@naver.com>
2023-12-07 14:00:39 +0900

    add POST PUT DELETE of boardApi

--- handler/board.go
+++ handler/board.go
@@ -11,6 +11,9 @@ func Board(base *gin.RouterGroup) {
 	board := base.Group("/board")
 	{
 		board.GET("", getBoard)
+		board.POST("", addBoard)
+		board.PUT("/:board_id", editBoard)
+		board.DELETE("/delete/:board_id", removeBoard)
 	}
 }
 
@@ -22,4 +25,38 @@ func getBoard(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, obj)
+}
+
+func addBoard(ctx *gin.Context) {
+	obj := new(dto.Board)
+	if err := ctx.Bind(obj); err != nil {
+		ctx.Status(http.StatusUnprocessableEntity)
+	} else {
+		// TODO : insert
+
+		ctx.JSON(http.StatusOK, nil)
+	}
+}
+
+func editBoard(ctx *gin.Context) {
+	obj := new(dto.Board)
+	if err := ctx.Bind(obj); err != nil {
+		ctx.Status(http.StatusUnprocessableEntity)
+	} else {
+		// TODO : insert
+
+		ctx.JSON(http.StatusOK, nil)
+	}
+}
+
+func removeBoard(ctx *gin.Context) {
+	val := ctx.Request.URL.Query().Get("board_id")
+	if val == "" {
+		ctx.Status(http.StatusUnprocessableEntity)
+		return
+	}
+
+	// TODO : delete
+
+	ctx.Status(http.StatusOK)
 }
