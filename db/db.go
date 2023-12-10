11e0dcdd57512e3c6144e243dba75b72d5db1539
east-true <leik24@naver.com>
2023-12-10 23:16:26 +0900

    add sql parser func

--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"strings"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
@@ -22,10 +23,19 @@ func GetInstance() (*sql.Conn, error) {
 	return instance.db.Conn(ctx)
 }
 
+func Parse(sql string) string {
+	sqlLines := strings.Split(sql, "\n")
+	for i, line := range sqlLines {
+		sqlLines[i] = strings.TrimSpace(line)
+	}
+	return strings.Join(sqlLines, " ")
+}
+
 type DB struct {
-	User        string
-	Password    string
-	Destination string
+	User         string
+	Password     string
+	Destination  string
+	QueryTimeout time.Duration
 
 	db *sql.DB
 }
