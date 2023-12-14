b49f2649666731d8d15d6250505fc59bb895a95d
east-true <leik24@naver.com>
2023-12-14 11:46:21 +0900

    add AuthToken struct

---
+++ server/helper/token/claim.go
@@ -0,0 +1,67 @@
+package token
+
+import (
+	"context"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/redis/go-redis/v9"
+)
+
+type Claims struct {
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func NewClaims(uuid, role string, now time.Time, dur time.Duration) *Claims {
+	return &Claims{
+		Role: role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   uuid,
+			IssuedAt:  jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(dur)),
+		},
+	}
+}
+
+func (c *Claims) Token() (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
+	return t.SignedString(secret)
+}
+
+func (c *Claims) Verify(token string) bool {
+	jwtToken, err := jwt.ParseWithClaims(token, c, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); ok {
+			return secret, nil
+		}
+
+		return nil, jwt.ErrInvalidKeyType
+	})
+	if err != nil {
+		return false
+	}
+
+	return jwtToken.Valid
+}
+
+func (c *Claims) Expired() bool {
+	return c.ExpiresAt.Time.After(time.Now())
+}
+
+func (c *Claims) store() error {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	conn := rdb.Conn()
+	defer conn.Close()
+
+	ctx := context.Background()
+	err := conn.Set(ctx, c.Subject, c, c.ExpiresAt.Sub(time.Now())).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ server/helper/token/jwt.go
@@ -0,0 +1,81 @@
+package token
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var secret []byte
+
+func init() {
+	idgen, _ := uuid.NewRandom()
+	id := idgen.String()
+	secret = []byte(id)
+}
+
+func JwtVerify(ctx *gin.Context) {
+	auth := ctx.Request.Header.Get("authorization")
+	if strings.HasPrefix(auth, "Bearer") {
+		token := strings.Split(auth, " ")[1]
+		claim := new(Claims)
+		if claim.Verify(token) {
+			return
+		}
+	}
+
+	ctx.AbortWithStatus(http.StatusForbidden)
+}
+
+type AuthToken struct {
+	Access  *Claims `json:"access_token"`
+	refresh *Claims
+}
+
+func NewAuthToken(role string) *AuthToken {
+	now := time.Now()
+	idgen, _ := uuid.NewUUID()
+	id := idgen.String()
+	access := NewClaims(id, role, now, 10*time.Minute)
+	refresh := NewClaims(id, role, now, 1*time.Hour)
+	if err := refresh.store(); err != nil {
+		return nil
+	}
+
+	return &AuthToken{
+		Access:  access,
+		refresh: refresh,
+	}
+}
+
+func (auth *AuthToken) GetTokens() (string, string, error) {
+	access, err := auth.Access.Token()
+	if err != nil {
+		return "", "", err
+	}
+
+	refresh, err := auth.refresh.Token()
+	if err != nil {
+		return access, "", err
+	}
+
+	return access, refresh, nil
+}
+
+func (auth *AuthToken) Refresh() (string, error) {
+	if auth.refresh == nil {
+		return "", errors.New("not issued refresh token")
+	}
+
+	if auth.refresh.Expired() {
+		return "", jwt.ErrTokenExpired
+	}
+
+	auth.Access = NewClaims(auth.refresh.Subject, auth.refresh.Role, time.Now(), 10*time.Minute)
+	return auth.Access.Token()
+}
