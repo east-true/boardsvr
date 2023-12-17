34cc2b9c2bf826c99f0ac136b269b37cfa63b38c
east-true <leik24@naver.com>
2023-12-18 01:08:57 +0900

    add redis example test code

---
+++ db/redis/redis_test.go
@@ -0,0 +1,43 @@
+package redis_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func TestExampleClient(t *testing.T) {
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		t.Log("key2 does not exist")
+	} else if err != nil {
+		t.Error(err)
+		return
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
